9ca77fdfaf33e720e522b03bb1b1830c669a9769
Ricky Clarkson <ricky.clarkson@gmail.com>
2020-12-01 23:08:49 -0800

    Day 2 part 1

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,47 @@ fn day1() {
     }
 }
 
+fn day2() {
+    use std::str::FromStr;
+    use std::convert::TryInto;
+
+    struct PolicyAndPassword {
+        min_occurrences: u32,
+        max_occurrences: u32,
+        letter: char,
+        password: String
+    }
+
+    impl PolicyAndPassword {
+        fn is_valid(&self) -> bool {
+            let num_occurrences = self.password.chars().filter(|c| *c == self.letter).count();
+            num_occurrences >= self.min_occurrences.try_into().unwrap() && num_occurrences <= self.max_occurrences.try_into().unwrap()
+        }
+    }
+    impl FromStr for PolicyAndPassword {
+        type Err = std::num::ParseIntError;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            use regex::Regex;
+            let re = Regex::new(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)$").unwrap();
+            let caps = re.captures(s).unwrap();
+
+            Ok(PolicyAndPassword {
+                min_occurrences: caps.get(1).unwrap().as_str().parse::<u32>().unwrap(),
+                max_occurrences: caps.get(2).unwrap().as_str().parse::<u32>().unwrap(),
+                letter: caps.get(3).unwrap().as_str().chars().nth(0).unwrap(),
+                password: caps.get(4).unwrap().as_str().to_string()
+            })
+        }
+    }
+
+    let num_valid = read_a_file("./day2_passwords.txt").unwrap().iter()
+        .map(move |line| PolicyAndPassword::from_str(line).unwrap())
+        .filter(move |policy_and_password| policy_and_password.is_valid())
+        .count();
+    print!("Day 2, there are {} valid passwords.\n", num_valid);
+}
+
 fn main() {
-    day1();
+    day2();
 }
