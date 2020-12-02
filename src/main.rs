5d1b1ef6fd88338225680ead098295ea115d5132
Ricky Clarkson <ricky.clarkson@gmail.com>
2020-12-01 23:20:12 -0800

    Day 2, part 2

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,13 @@ fn day2() {
             let num_occurrences = self.password.chars().filter(|c| *c == self.letter).count();
             num_occurrences >= self.min_occurrences.try_into().unwrap() && num_occurrences <= self.max_occurrences.try_into().unwrap()
         }
+
+        fn is_valid_part2(&self) -> bool {
+            let valid_position1 = self.min_occurrences - 1;
+            let valid_position2 = self.max_occurrences - 1;
+            (self.password.chars().nth(valid_position1.try_into().unwrap()).unwrap() == self.letter) !=
+                (self.password.chars().nth(valid_position2.try_into().expect("Couldn't convert valid_position2")).unwrap() == self.letter)
+        }
     }
     impl FromStr for PolicyAndPassword {
         type Err = std::num::ParseIntError;
@@ -64,7 +71,13 @@ fn day2() {
         .map(move |line| PolicyAndPassword::from_str(line).unwrap())
         .filter(move |policy_and_password| policy_and_password.is_valid())
         .count();
-    print!("Day 2, there are {} valid passwords.\n", num_valid);
+    print!("Day 2 part 1: There are {} valid passwords.\n", num_valid);
+
+    let num_valid = read_a_file("./day2_passwords.txt").unwrap().iter()
+        .map(move |line| PolicyAndPassword::from_str(line).unwrap())
+        .filter(move |policy_and_password| policy_and_password.is_valid_part2())
+        .count();
+    print!("Day 2 part 2: There are {} valid passwords.\n", num_valid);
 }
 
 fn main() {
