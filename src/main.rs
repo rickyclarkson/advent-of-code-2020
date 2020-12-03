98d92840fecb21278ad3c14cf1458c8ba7cc48ae
Ricky Clarkson <ricky.clarkson@gmail.com>
2020-12-03 00:11:06 -0800

    Day 3 part 2

--- src/main.rs
+++ src/main.rs
@@ -112,15 +112,29 @@ fn day2() {
 
 fn day3() {
   let lines = read_a_file("./day3_trees.txt").unwrap();
-  let mut trees = 0;
-  let mut x = 0;
-  for line in lines {
-    if line.chars().nth(x % line.chars().count()).unwrap() == '#' {
-        trees += 1;
+
+  fn count_trees(lines: &Vec<String>, horizontal_speed: usize, vertical_speed: usize) -> usize {
+    let mut x = 0;
+    let mut y = 0;
+    let mut trees = 0;
+    while y < lines.len() {
+        let line = &lines[y];
+        if line.chars().nth(x % line.chars().count()).unwrap() == '#' {
+            trees += 1;
+        }
+        x += horizontal_speed;
+        y += vertical_speed;
     }
-    x += 3;
+    trees
   }
-  print!("Day 3: We encounter {} trees\n", trees);
+
+  print!("Day 3, part 1: We encounter {} trees\n", count_trees(&lines, 3, 1));
+  print!("Day 3, part 2: Multiplied trees: {}\n",
+         count_trees(&lines, 1, 1) *
+         count_trees(&lines, 3, 1) *
+         count_trees(&lines, 5, 1) *
+         count_trees(&lines, 7, 1) *
+         count_trees(&lines, 1, 2));
 }
 
 fn main() {
