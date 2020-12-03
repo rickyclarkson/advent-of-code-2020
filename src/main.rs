6f9d4364df1ca194107190110e340139aa67c996
Ricky Clarkson <ricky.clarkson@gmail.com>
2020-12-03 00:11:30 -0800

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -111,30 +111,35 @@ fn day2() {
 }
 
 fn day3() {
-  let lines = read_a_file("./day3_trees.txt").unwrap();
+    let lines = read_a_file("./day3_trees.txt").unwrap();
 
-  fn count_trees(lines: &Vec<String>, horizontal_speed: usize, vertical_speed: usize) -> usize {
-    let mut x = 0;
-    let mut y = 0;
-    let mut trees = 0;
-    while y < lines.len() {
-        let line = &lines[y];
-        if line.chars().nth(x % line.chars().count()).unwrap() == '#' {
-            trees += 1;
+    fn count_trees(lines: &Vec<String>, horizontal_speed: usize, vertical_speed: usize) -> usize {
+        let mut x = 0;
+        let mut y = 0;
+        let mut trees = 0;
+        while y < lines.len() {
+            let line = &lines[y];
+            if line.chars().nth(x % line.chars().count()).unwrap() == '#' {
+                trees += 1;
+            }
+            x += horizontal_speed;
+            y += vertical_speed;
         }
-        x += horizontal_speed;
-        y += vertical_speed;
+        trees
     }
-    trees
-  }
 
-  print!("Day 3, part 1: We encounter {} trees\n", count_trees(&lines, 3, 1));
-  print!("Day 3, part 2: Multiplied trees: {}\n",
-         count_trees(&lines, 1, 1) *
-         count_trees(&lines, 3, 1) *
-         count_trees(&lines, 5, 1) *
-         count_trees(&lines, 7, 1) *
-         count_trees(&lines, 1, 2));
+    print!(
+        "Day 3, part 1: We encounter {} trees\n",
+        count_trees(&lines, 3, 1)
+    );
+    print!(
+        "Day 3, part 2: Multiplied trees: {}\n",
+        count_trees(&lines, 1, 1)
+            * count_trees(&lines, 3, 1)
+            * count_trees(&lines, 5, 1)
+            * count_trees(&lines, 7, 1)
+            * count_trees(&lines, 1, 2)
+    );
 }
 
 fn main() {
