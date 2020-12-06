c2c86c3f0c0c219954bb3849490230d1a5772d42
Ricky Clarkson <ricky.clarkson@gmail.com>
2020-12-06 12:05:12 -0800

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -262,27 +262,37 @@ fn day6() {
     use std::collections::HashSet;
 
     struct Group {
-        lines: Vec<String>
+        lines: Vec<String>,
     }
 
     impl Group {
         fn count_yeses(&self) -> usize {
-            self.lines.iter().flat_map(|line| line.chars()).collect::<HashSet<char>>().len()
+            self.lines
+                .iter()
+                .flat_map(|line| line.chars())
+                .collect::<HashSet<char>>()
+                .len()
         }
     }
 
     let lines = read_a_file("./day6_customs_forms.txt").unwrap();
-    let mut groups : Vec<Group> = vec!();
-    groups.push(Group { lines: vec!() });
+    let mut groups: Vec<Group> = vec![];
+    groups.push(Group { lines: vec![] });
     for line in lines {
         if line.is_empty() {
-            groups.push(Group { lines: vec!() });
+            groups.push(Group { lines: vec![] });
         } else {
             groups.last_mut().unwrap().lines.push(line);
         }
     }
 
-    print!("Total number of yeses, {}\n", groups.iter().map(|group| group.count_yeses()).sum::<usize>());
+    print!(
+        "Total number of yeses, {}\n",
+        groups
+            .iter()
+            .map(|group| group.count_yeses())
+            .sum::<usize>()
+    );
 }
 
 fn main() {
