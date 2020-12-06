20ae9c178def335499e63caa33f9ea16d06b0716
Ricky Clarkson <ricky.clarkson@gmail.com>
2020-12-06 10:38:35 -0800

    Day 5, part 2

--- src/main.rs
+++ src/main.rs
@@ -237,8 +237,7 @@ fn day4() {
 
 fn day5() {
     let lines = read_a_file("./day5_boarding_passes.txt").unwrap();
-    print!(
-        "Highest seat number: {}\n",
+    let mut seat_numbers : Vec<u32> =
         lines
             .iter()
             .map(|line| line
@@ -247,9 +246,16 @@ fn day5() {
                 .replace('L', "0")
                 .replace('R', "1"))
             .map(|bin| u32::from_str_radix(&bin, 2).unwrap())
-            .max()
-            .unwrap()
-    );
+            .collect();
+            
+    seat_numbers.sort();
+
+    for (index, number) in seat_numbers.iter().enumerate() {
+        if seat_numbers[(index + 1) as usize] != number + 1 {
+            print!("Your seat number is {}\n", number + 1);
+            return;
+        }
+    }
 }
 
 fn main() {
